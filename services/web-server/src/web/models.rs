fb74c985076b898caea259e40c08978d8e66b2ed
Siya Nkuna <sudosprogrammer@gmail.com>
2024-07-03 15:28:20 +0200

    add get all users endpoint

--- services/web-server/src/web/models.rs
+++ services/web-server/src/web/models.rs
@@ -3,6 +3,16 @@ use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 use validator::Validate;
 
+#[derive(Debug, Serialize, Deserialize, JsonSchema, sqlx::Type)]
+#[sqlx(type_name = "loan_status_enum")]
+#[sqlx(rename_all = "lowercase")] 
+pub enum LoanStatus {
+	Pending, 
+	Approved, 
+	Rejected, 
+	Completed
+}
+
 #[derive(Serialize, Deserialize, JsonSchema, Validate, Debug)]
 pub struct LoanRequest {
 	#[validate(length(min = 1, message = "Can not be empty"))]
@@ -18,7 +28,6 @@ pub(crate) struct LoanRequestUuid {
 	pub loan_request: LoanRequest,
 }
 
-
 #[derive(Deserialize, Serialize, JsonSchema)]
 pub struct DataResult<T> where T: Serialize
 {
@@ -27,8 +36,18 @@ pub struct DataResult<T> where T: Serialize
 	pub errors: Option<Vec<String>>,
 }
 
+#[derive(Deserialize, Serialize, JsonSchema, sqlx::FromRow)]
+pub struct PersonaDetails {
+	pub persona_id: Option<String>,
+	pub loan_status: Option<LoanStatus>,
+	pub installment_amount_cents: Option<i32>,
+	pub interest_rate: Option<f32>
+}
+
 #[derive(Deserialize, Serialize, JsonSchema)]
-pub struct Persona {
-	pub persona_id: u32,
-	pub name: String,
+pub struct PersonaResult {
+	pub success: bool,
+	pub data: Option<Vec<PersonaDetails>>,
+	pub errors: Option<String>,
+	pub flex: String
 }
